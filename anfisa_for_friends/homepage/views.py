5b052fa4f713369a8a57bc1a3772be837ca490ca
Roman_Omelshin <stas.basov@whatever.com>
2026-01-14 15:09:37 +0300

    add commit 1

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from django.db.models import Q 
+from ice_cream.models import IceCream
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.select_related('wrapper').values('id', 'title', 'description','wrapper__title').filter(Q(is_published=True)&Q(is_on_main=True)).order_by('title')
+    context = {
+        'ice_cream_list': ice_cream_list,        
+    }
+    return render(request, template_name, context)
--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from django.http import Http404
 
+from ice_cream.models import IceCream
 
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
-    context = {}
-    return render(request, template, context)
+    # Готовимся перехватить исключение. 
+    try:
+        # Пытаемся получить объект с заданным pk:
+          ice_cream = IceCream.objects.get(pk=pk)
+    # Если объект не найден...
+    except IceCream.DoesNotExist:
+        # ...выбрасываем исключение Http404
+        raise Http404('Такого мороженого не существует.')
+        # Специальный обработчик перехватит выброшенное исключение
+        # и среагирует установленным образом; 
+        # по умолчанию - вернёт пользователю стандартную страницу ошибки 404.
+    
+    context = {
+        'ice_cream': ice_cream,
+    }
+    return render(request, template, context) 
 
 
 def ice_cream_list(request):
